562ec3f9688e94c64fcedc86a7b26feacc854fae
Jurnell Cockhren <jurnell@civichacker.com>
2023-03-10 12:22:15 -0600

    Incorporating updates from Dart SDK 2.0 release

--- lib/tiki_sdk_js.dart
+++ lib/tiki_sdk_js.dart
@@ -4,7 +4,6 @@ import 'dart:html' as html;
 import 'package:js/js.dart';
 
 export 'package:tiki_sdk_dart/node/key/key_storage.dart';
-export 'package:tiki_sdk_dart/helpers/sdk_builder.dart';
 
 @JS('cryptosupported')
 external set _hasCryptoSupport(bool Function() f);
--- web/main.dart
+++ web/main.dart
@@ -1,22 +1,18 @@
 
 import 'dart:html' as html;
 import 'package:js/js.dart';
+import 'dart:typed_data';
+
 
 import 'package:http/http.dart' as http;
 import 'package:sqlite3/wasm.dart';
 
 import 'package:tiki_sdk_js/tiki_sdk_js.dart';
 import 'package:tiki_sdk_dart/tiki_sdk.dart';
-
-import 'package:tiki_sdk_dart/cache/consent/consent_service.dart';
-import 'package:tiki_sdk_dart/cache/ownership/ownership_service.dart';
-import 'package:tiki_sdk_dart/l0/storage/storage_service.dart';
-import 'package:tiki_sdk_dart/node/backup/backup_service.dart';
-import 'package:tiki_sdk_dart/node/block/block_service.dart';
 import 'package:tiki_sdk_dart/node/key/key_service.dart';
-import 'package:tiki_sdk_dart/node/node_service.dart';
-import 'package:tiki_sdk_dart/node/transaction/transaction_service.dart';
 import 'package:tiki_sdk_dart/node/key/key_storage.dart';
+import 'package:tiki_sdk_dart/node/key/key_model.dart';
+import 'package:tiki_sdk_dart/node/key/key_repository.dart';
 
 @JS('___TikiSdk__hasCrypto') // Name of the function in JS
 external set _hasCryptoSupport(bool Function() f);
@@ -25,13 +21,33 @@ external set _hasCryptoSupport(bool Function() f);
 external set _sqliteVersion(String Function(WasmSqlite3) f);
 
 @JS('___TikiSdk__hasWallet')
-external set _hasWallet(bool Function() f);
+external set _hasWallet(bool Function(String) f);
+
+@JS('___TikiSdk__SdkInit')
+external set _sdkInit(Future<String> Function(String, String) f);
+
+void _initializeDatabase(VersionChangeEvent e) {
+ print("Initializing DB!");
+}
+
+Future<String> __sdkInit(String publishingId, String _address) async {
+  InMemKeyStorage mem = new InMemKeyStorage();
+  //WasmSqlite3 s = await loadSqlite();
+  Future cdb = html.window.indexedDB.open("mydb",
+    version: 1,
+      onUpgradeNeeded: _initializeDatabase);
+  KeyService kservice = new KeyService(mem);
+  return await TikiSdk.withAddress(mem, address: _address);
+}
 
 bool _internalCrypto() {
   return html.Crypto.supported;
 }
 
-bool _internalHasWallet() {
+bool _internalHasWallet(String _address) {
+  InMemKeyStorage mem = new InMemKeyStorage();
+  KeyService kservice = new KeyService(mem);
+  Future<KeyModel?> model = kservice.get(_address);
   return false;
 }
 
@@ -39,8 +55,6 @@ String _internalSqliteVersion(WasmSqlite3 db) {
   return db.version.libVersion;
 }
 
-
-/// A ephemeral key-value storage.
 class InMemKeyStorage extends KeyStorage {
   Map<String, String> storage = {};
 
@@ -50,66 +64,6 @@ class InMemKeyStorage extends KeyStorage {
   @override
   Future<void> write({required String key, required String value}) async =>
       storage[key] = value;
-}
-
-class TikiSdkJsBuilder implements SdkBuilder {
-  String? _origin;
-  KeyStorage? _keyStorage;
-  String? _databaseDir;
-  String? _publishingId;
-  String? _address;
-
-  @override
-  void origin(String origin) => _origin = origin;
-
-  @override
-  void keyStorage(KeyStorage keyStorage) => _keyStorage = keyStorage;
-  @override
-  void databaseDir(String databaseDir) => _databaseDir = databaseDir;
-  @override
-  void publishingId(String? publishingId) => _publishingId = publishingId;
-  @override
-  void address(String? address) => _address = address;
-
-  Future<TikiSdk> build() async {
-    KeyModel primaryKey = await _loadPrimaryKey();
-    WasmSqlite3 sqlite3 = await loadSqlite();
-    CommonDatabase database = sqlite3
-        .open("$_databaseDir/${Bytes.base64UrlEncode(primaryKey.address)}.db");
-
-    StorageService l0Storage =
-        StorageService.publishingId(primaryKey.privateKey, _publishingId!);
-
-    NodeService nodeService = NodeService()
-      ..blockInterval = const Duration(minutes: 1)
-      ..maxTransactions = 200
-      ..transactionService = TransactionService(database)
-      ..blockService = BlockService(database)
-      ..primaryKey = primaryKey;
-    nodeService.backupService =
-        BackupService(l0Storage, database, primaryKey, nodeService.getBlock);
-    await nodeService.init();
-
-    OwnershipService ownershipService =
-        OwnershipService(_origin!, nodeService, nodeService.database);
-    ConsentService consentService =
-        ConsentService(nodeService.database, nodeService);
-    return TikiSdk(ownershipService, consentService, nodeService);
-  }
-
-  Future<KeyModel> _loadPrimaryKey() async {
-    if (_keyStorage == null) {
-      throw Exception('Keystore must be set to build NodeService');
-    }
-    KeyService keyService = KeyService(_keyStorage!);
-    if (_address != null) {
-      KeyModel? key = await keyService.get(_address!);
-      if (key != null) {
-        return key;
-      }
-    }
-    return await keyService.create();
-  }
 }
 
 Future<WasmSqlite3> loadSqlite() async {
@@ -129,15 +83,7 @@ void main() async {
   _hasCryptoSupport = allowInterop(_internalCrypto);
   _sqliteVersion = allowInterop(_internalSqliteVersion);
   _hasWallet = allowInterop(_internalHasWallet);
+  _sdkInit = allowInterop(__sdkInit);
 
-  TikiSdkJsBuilder builder = TikiSdkJsBuilder();
-  builder.origin('com.exaammple.myproduct');
-  builder.databaseDir('path/to/database');
-  builder.keyStorage(InMemKeyStorage());
-
-  builder.publishingId("api_key_from_mytiki.com");
-  builder.address('<user_address>');
-
-  TikiSdk sdk = await builder.build();
 
 }

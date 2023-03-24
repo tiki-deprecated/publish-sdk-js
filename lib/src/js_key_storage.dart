a2e5ff1a7c3cbd28e5022992fee172742bcdd31f
Jurnell Cockhren <jurnell@civichacker.com>
2023-03-23 20:51:35 -0500

    new class for web based KeyStorage; WIP

--- lib/src/js_key_storage.dart
+++ lib/src/js_key_storage.dart
@@ -1,18 +1,55 @@
 library js_key_storage;
 
 import 'dart:html' as html;
-import 'dart:indexed_db' as db;
+import 'dart:indexed_db';
 import 'package:js/js.dart';
-import '../tiki_sdk_dart/node/key/key_storage.dart';
+import 'package:tiki_sdk_dart/node/key/key_storage.dart';
 
-class JSKeyStorage implements KeyStorage {
+import 'dart:js_util';
 
-  final Future<Database> indexedDb = html.window.indexedDB.open('com.mytiki');
-  Future<void> write({required String key, required String value}) async {
-    ObjectStore ostore = indexedDB.createObjectStore('com.mytiki')
+class IndexedDBKeyStorage implements KeyStorage {
+  static const String KEY_STORE = 'com.mytiki';
+  static const String DB_NAME = 'TikiDB';
+
+  final IdbFactory? _store = html.window.indexedDB;
+  ObjectStore? ostore;
+  Database? __database;
+
+  Future? openDB() async {
+    return await _store?.open(DB_NAME,
+      onUpgradeNeeded: _initializeDatabase)
+    .then(_loadFromDB);
   }
-  Future<String?> read({required String key}) async {
-    Transaction transaction = indexedDb.transaction(["com.mytiki"], "readonly");
+
+  set database(Database value) => __database = value;
+
+  void _initializeDatabase(VersionChangeEvent e) {
+    print("Initializing DB!");
+    Database __db = (e.target as Request).result;
+    try {
+      final store = __db.transaction(KEY_STORE, 'readwrite').objectStore(KEY_STORE);
+    } on Exception {
+      __db.createObjectStore(KEY_STORE, autoIncrement: true);
+    }
+  }
+
+  Future _loadFromDB(Database db) {
+    __database = db;
+    return new Future.value(db);
+  }
+
+
+  Future<void> write(String key, String value) async {
+    database = await openDB();
+    Transaction? transaction = __database?.transaction(KEY_STORE, "readwrite");
+    transaction?.objectStore(KEY_STORE).add(value, key);
+  }
+
+  Future<String?> read(String key) async {
+    __database = await openDB();
+    Transaction? transaction = __database?.transaction(KEY_STORE, "readonly");
+    var store = transaction?.objectStore(KEY_STORE);
+    return await store?.getObject(key);
 
   }
 }
--- lib/tiki_sdk_js.dart
+++ lib/tiki_sdk_js.dart
@@ -5,6 +5,8 @@ import 'package:js/js.dart';
 
 export 'package:tiki_sdk_dart/node/key/key_storage.dart';
 
+export 'src/js_key_storage.dart';
+
 @JS('cryptosupported')
 external set _hasCryptoSupport(bool Function() f);
 
